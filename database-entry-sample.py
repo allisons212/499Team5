13f64f05b3664ddaba70f2ba24ec59312f4ffc79
DevinPate1 <devinpatel02@gmail.com>
2022-09-17 01:59:47 -0500

    Moved relative paths and URLs to local .env file

--- database-entry-sample.py
+++ database-entry-sample.py
@@ -1,15 +1,22 @@
 import os
+from dotenv import load_dotenv
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
+# Loads virtual environment variables
+load_dotenv()
+
 # Path pointing to a downloaded private key from Project Settings > Service Accounts
-credentials_path = os.path.join("authentication", "firebase-sdk.json")
+credentials_path = os.environ.get('credentials_path')
+
+# Reads in url to Realtime Database
+database_url = os.environ.get('database_url')
 
 # Sets up credentials and initializes realtime database URL
 cred = credentials.Certificate(credentials_path)
 firebase_admin.initialize_app(cred, {
-    'databaseURL' : 'https://coursescheduler499-default-rtdb.firebaseio.com/'
+    'databaseURL' : database_url
 })
 
 
