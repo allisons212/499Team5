a04e27c4b53cfb969c86ab3853eaf40e0c8ebbd0
DevinPate1 <devinpatel02@gmail.com>
2022-09-19 20:27:22 -0500

    Added Room, Time, and Day assignments to database import

--- DataOperation/testDataOperation.py
+++ DataOperation/testDataOperation.py
@@ -3,14 +3,17 @@ from DataOperationException import * # Custom exceptions
 
 
 def main():
-    csv_file = "ClassData/Dept2ClassData.csv" # This is provided from GUI
-    department_abbr = "ECE"                   # This is provided from GUI
 
     data_operation = DataOperation() # Runs constructor to authenticate credentials
     
     # Sample try-catch block for importCSV()
     try:
+        csv_file = "ClassData/Dept2ClassData.csv" # This is provided from GUI
+        department_abbr = "ECE"                   # This is provided from GUI
         data_operation.importCSV(csv_file, department_abbr) # Test a csv file
+    
+    except FileNotFoundError as fnfe:
+        print(f"{fnfe}\n")
         
     except ImportFormatError as ife:
         print(f"{ife}\n")
