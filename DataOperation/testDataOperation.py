76891a9f49886c7ef0c786c2cddd782bcba8897b
DevinPate1 <devinpatel02@gmail.com>
2022-09-19 20:02:52 -0500

    Updated Dept2 CSV, reorganized file structure

--- DataOperation/testDataOperation.py
+++ DataOperation/testDataOperation.py
@@ -3,13 +3,14 @@ from DataOperationException import * # Custom exceptions
 
 
 def main():
-    csv_file = "ClassData/Dept1ClassData.csv" # This is provided from GUI
+    csv_file = "ClassData/Dept2ClassData.csv" # This is provided from GUI
+    department_abbr = "ECE"                   # This is provided from GUI
 
     data_operation = DataOperation() # Runs constructor to authenticate credentials
     
     # Sample try-catch block for importCSV()
     try:
-        data_operation.importCSV(csv_file, "CS") # Test a csv file
+        data_operation.importCSV(csv_file, department_abbr) # Test a csv file
         
     except ImportFormatError as ife:
         print(f"{ife}\n")
