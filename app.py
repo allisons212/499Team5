81a6fa4afbf7661eb78f9e3321c57023706a8190
allisons212 <73364759+allisons212@users.noreply.github.com>
2022-10-10 17:13:32 -0500

    login page added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask_navigation import Navigation
+from flask import Flask, render_template, redirect, url_for, request
+from flask_navigation import Navigation #pip install flask_navigation
 
 app = Flask(__name__)
 nav = Navigation(app)
@@ -13,10 +13,23 @@ nav.Bar('top', [
     nav.Item('generateSchedule', 'generate_schedule'),
     nav.Item('Settings', 'settings'),
     nav.Item('uploadCSV', 'upload_csv'),
+    nav.Item('Login', 'login'),
 ])
 
 
-@app.route('/')  # this block is for the default page
+# Route for handling the login page logic
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/index')
 def home():
     return render_template('index.html')
 
