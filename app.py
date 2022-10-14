211ea40cb688dfbab048207d9223980cbba774a2
faith-grimmeisen <92333932+faith-grimmeisen@users.noreply.github.com>
2022-10-13 19:28:37 -0500

    changed homepage to generate schedule

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def login():
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
-            return redirect(url_for('home'))
+            return redirect(url_for('generate_schedule'))
     return render_template('login.html', error=error)
 
 
