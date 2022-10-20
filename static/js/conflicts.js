d28344e6684fa8e5d0939a12126293bb957c69e6
faith-grimmeisen <92333932+faith-grimmeisen@users.noreply.github.com>
2022-10-20 18:49:43 -0500

    Started on FAQ

--- static/js/conflicts.js
+++ static/js/conflicts.js
@@ -19,9 +19,3 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
