5dd8e092613a43207c200b2bd62fc335eaa9a88f
Harrison Matthews <hmatthews070@gmail.com>
2022-11-05 23:04:13 -0500

    Removed print statement

--- app.py
+++ app.py
@@ -183,7 +183,6 @@ def upload_csv():
             if errorCount > 0:
                 if(errorCount == 1 and error.find('\n')):
                     error = error.replace('\n','')
-                print(error)
                     
                 error = error.split('\n')
                 return render_template('uploadCSV.html', department=user.getUser(), departmentManual=departmentManual, rooms=rooms, error=error)
